5544f41058bf64a69a1f0c9b2933b52c84db12a8
tstuefe <thomas.stuefe@gmail.com>
2025-08-12 08:08:36 +0200

    wip

--- jtreg-runs/jtreg-17u/setenv.sh
+++ jtreg-runs/jtreg-17u/setenv.sh
@@ -3,5 +3,5 @@
 export OPENJDK_ROOT=/shared/projects/openjdk
 
 export JT_JAVA=${OPENJDK_ROOT}/jdks/sapmachine17/
-export PATH=${OPENJDK_ROOT}/jtreg-prebuilt/jtreg-6.2/bin/:$PATH
+export PATH=${OPENJDK_ROOT}/jtreg-prebuilt/jtreg-7.3.1/bin/:$PATH
 
