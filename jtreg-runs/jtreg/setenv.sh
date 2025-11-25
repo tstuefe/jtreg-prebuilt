e682d2201000ee1202ed2d47a8e1387d572bcaae
tstuefe <thomas.stuefe@gmail.com>
2025-11-25 07:33:37 +0100

    update JT_JAVA

--- jtreg-runs/jtreg/setenv.sh
+++ jtreg-runs/jtreg/setenv.sh
@@ -1,5 +1,5 @@
 export OPENJDK_ROOT=/shared/projects/openjdk
 
-export JT_JAVA=${OPENJDK_ROOT}/jdks/sapmachine11
+export JT_JAVA=${OPENJDK_ROOT}/jdks/sapmachine25
 export PATH=${OPENJDK_ROOT}/jtreg-prebuilt/jtreg/bin/:$PATH
 
