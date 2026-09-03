965c9c908b28e7d08b3a67c30ac6c81228d3c8de
tstuefe <thomas.stuefe@gmail.com>
2026-09-03 10:48:20 +0200

    wip

--- unpack-all.sh
+++ unpack-all.sh
@@ -6,6 +6,20 @@ set -euo pipefail
 
 trap 'echo "Error on line $LINENO (exit code $?)" >&2' ERR
 
-for a in jtreg-*.tar.gz; do tar -xf $a; mv jtreg ${a/\.tar\.gz//}; done
-for a in jtreg-*.zip; do unzip $a; mv jtreg ${a/\.zip//}; done
+for a in jtreg-*.tar.gz; do
+	into="${a/\.tar\.gz//}";
+	if [[ -d "$into" ]]; then
+		echo "already exists"
+	fi
+	tar -xf $a; mv jtreg $into;
+done
+
+for a in jtreg-*.zip; do
+	into="${a/\.zip//}";
+	if [[ -d "$into" ]]; then
+		echo "already exists"
+	fi
+	tar -xf $a; mv jtreg $into;
+done
+
 
